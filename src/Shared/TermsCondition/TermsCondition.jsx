f024f21fb59d159e1d3ac26787d6f7587955e484
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2025-01-29 09:42:16 +0600

    Dynamic pages has been responsive done

--- src/Shared/TermsCondition/TermsCondition.jsx
+++ src/Shared/TermsCondition/TermsCondition.jsx
@@ -35,10 +35,10 @@ const TermsCondition = () => {
 
   return (
     <section className="text-center mt-8 xs:mt-8 sm:mt-8 md:mt-8 lg:mt-8 xl:mt-16 2xl:mt-16 3xl:mt-16 mb-[70px] h-auto">
-      <h1 className="text-center text-3xl xs:text-3xl sm:text-3xl md:text-3xl lg:text-3xl xl:text-5xl 2xl:text-5xl 3xl:text-5xl font-semibold">
+      <h1 className="text-center text-3xl xs:text-3xl sm:text-3xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-5xl 3xl:text-5xl font-semibold">
         {data?.data?.page_title}
       </h1>
-      <div className="dynamic-page-wrapper text-left px-3 xs:px-3 sm:px-3 md:px-3 lg:px-3 xl:px-3 2xl:px-3 3xl:px-[200px]">
+      <div className="dynamic-page-wrapper text-left px-5 xs:px-5 sm:px-5 md:px-5 lg:px-7 xl:px-8 2xl:px-9 3xl:px-[100px] 4xl:px-[200px]">
         {parse(parsedData)}
       </div>
     </section>
